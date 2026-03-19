3b785e89b56d5b0d1f9c4bcff516328626c8ff0d
ethersoftexrambabu-blip <137375643+developerMaurya@users.noreply.github.com>
2026-03-19 17:16:19 +0530

    Revert "update"
    This reverts commit 08da82f4136d611833f112dc4da29fe19d96fdba.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.indiangold.medical"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.2.13676358"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
